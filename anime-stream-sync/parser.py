85c3251ff5fedd8f68e8618f8cb8bfe54df6afd1
naruto9156001 <naruto9156001@outlook.com>
2026-07-05 21:15:41 +0530

    Update parser.py

--- anime-stream-sync/parser.py
+++ anime-stream-sync/parser.py
@@ -1,50 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
-from config import TMDB_API_KEY
 
 def parse_anime_list(soup):
-    """Animesalt.in homepage se anime list"""
     items = []
-    # Animesalt ke common card selectors
-    cards = soup.select('.anime-card, .series, .item, a[href*="/anime/"]')
-    
-    for card in cards:
-        link_tag = card.find('a') or card
-        link = link_tag.get('href', '')
-        
-        if not link or '/episode/' in link:
-            continue
-            
-        title_tag = card.select_one('h3, .title, .name, img')
-        title = title_tag.get('alt') or title_tag.text.strip() if title_tag else ''
-        
-        if title and link:
-            full_link = link if link.startswith('http') else 'https://animesalt.in' + link
-            items.append({
-                'id': full_link.split('/')[-1],
-                'name': title,
-                'url': full_link,
-                'poster': ''
-            })
-    return items[:20]  # Limit for testing
+    # Stronger selectors for animesalt.in
+    for card in soup.select('a, div'):
+        link = card.get('href', '')
+        if '/anime/' in link or '/series/' in link:
+            title = card.get('title') or card.text.strip()
+            if len(title) > 3 and title not in ['Home', 'Anime', 'Login']:
+                full_url = link if link.startswith('http') else 'https://animesalt.in' + link
+                items.append({
+                    'id': full_url.split('/')[-1],
+                    'name': title,
+                    'url': full_url,
+                    'poster': ''
+                })
+    return list({v['url']:v for v in items}.values())  # remove duplicates
 
 def parse_episode_page(url):
-    """Episode page se player aur details"""
     try:
-        resp = requests.get(url, timeout=15)
-        soup = BeautifulSoup(resp.text, 'lxml')
-        
-        title = soup.find('h1')
-        title = title.text.strip() if title else 'Episode'
-        
-        # Player iframe dhundna
-        iframe = soup.find('iframe')
-        player_url = iframe.get('src') if iframe else url
+        r = requests.get(url, timeout=15)
+        soup = BeautifulSoup(r.text, 'lxml')
         
         return {
-            'title': title,
-            'thumbnail': '',
-            'player_url': player_url
+            'title': soup.find('h1').text.strip() if soup.find('h1') else 'Episode',
+            'player_url': url
         }
     except:
         return None
